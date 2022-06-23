fe383776d67352625c979c2ce3215d750199dee4
acgotaku <acgotaku311@gmail.com>
2022-06-23 23:37:23 +0900

    add todo view

---
+++ src/views/todo/Todo.tsx
@@ -0,0 +1,11 @@
+import styles from './todo.module.css';
+
+const TodoView = () => {
+  return (
+    <div className={styles.todo}>
+       <p>todo view</p>
+    </div>
+  );
+};
+
+export default TodoView;
