7dfbaa8f4dfc75345d6a7b2f3f02f14fec3c8f29
acgotaku <acgotaku311@gmail.com>
2022-06-23 23:10:47 +0900

    add mobx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(async ({ mode }) => {
+  return {
+    server: {
+      host: true,
+      port: 3400
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    plugins: [react()],
+    build: {
+      sourcemap: mode === 'production' ? 'hidden' : true,
+      emptyOutDir: true
+    }
+  };
+});
+
